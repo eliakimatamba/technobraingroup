88ca8baaacbd8d0bbd18150b2e9a760d7cd61afa
eliakimatamba <48688394+eliakimatamba@users.noreply.github.com>
2022-10-25 07:37:23 +0000

    spell check fix

--- question3_reverse_words.cpp
+++ question3_reverse_words.cpp
@@ -1,11 +1,10 @@
 /*
 Given an input string s, reverse the order of the words.
 A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
-Return a string of the words in reverse order concatanated by a single space.
+Return a string of the words in reverse order concatenated by a single space.
 
 Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have
-a single space separting the words.
-Do no include any extra spaces.
+a single space separating the words.
 */
 
 #include <iostream>
